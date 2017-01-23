30e63720b2a9827a45dc1f3a71cf0096f14f25a4
jdcroos <jd_komoon@hotmail.com>
2017-01-23 07:39:44 -0800

    fix word_tag_predictor

--- src/room007/models/word_tag_predictor.py
+++ src/room007/models/word_tag_predictor.py
@@ -25,13 +25,16 @@ TAG_STATS_IDX = pandas.Index(('present', 'total'))
 
 
 class Predictor(object):
-    def __init__(self):
+    def __init__(self, functional_test=False):
         self.vectorize = None
         self.feature_names = None
+        self.functional_test = functional_test
 
     def fit(self, train_data):
         print('start fitting')
         apply_preprocessing(train_data)
+        if self.functional_test:
+            train_data, throw_away = train_test_split(train_data, test_size=0.99)
         self.vectorizer = TfidfVectorizer(stop_words='english')
         train_features = self.vectorizer.fit_transform(train_data['titlecontent'])
         self.feature_names = self.vectorizer.get_feature_names()
@@ -40,11 +43,17 @@ class Predictor(object):
         print('start predicting')
         apply_preprocessing(test_dataframe)
         predictions = []
+        times = 0
         for entry in test_dataframe.to_dict(orient='records'):
-            test_features = self.vectorizer.transform([entry['titlecontent']])
-            entry = test_features.toarray()[0]
+            ## TODO: probably be made faster by using panda tricks and mass transform iso one transform per entry
+            times += 1
             number_of_tags = 3
-            prediction = [self.feature_names[i] for i, score in sorted(enumerate(entry), key=lambda t: t[1] * -1)[:number_of_tags]]
+            if self.functional_test and times > 10:
+                prediction = []
+            else:
+                test_features = self.vectorizer.transform([entry['titlecontent']])
+                entry = test_features.toarray()[0]
+                prediction = [self.feature_names[i] for i, score in sorted(enumerate(entry), key=lambda t: t[1] * -1)[:number_of_tags]]
             if 'tags' in entry:
                 diff_length = len(prediction) - len(entry['tags'])
                 if diff_length > 0:
@@ -73,15 +82,14 @@ def apply_preprocessing(data):
 def get_arguments():
     parser = argparse.ArgumentParser(description='Predict with it-idf.')
     parser.add_argument('--eval', help='apply to the testdata', action='store_true')
+    parser.add_argument('--test', help='run only on minimal part of data, to test functionality', action='store_true')
     args = parser.parse_args()
     return args
 
 
-def write_predictions(predictios):
-    f = open('test.out.csv', 'w')
-    f.write('"id","tags"\n')
-    for i, prediction in predictions:
-        f.write('"{}","{}"\n'.format(i, ' '.join(prediction)))
+def write_predictions(test_name, test_dataframe):
+    filename = '{}.out.csv'.format(test_name)
+    test_dataframe.to_csv(filename, columns=['id','tags'], index=False)
 
 
 def get_scoring(predictions, all_tags):
@@ -104,22 +112,29 @@ def main():
     data_info = info.CleanedData()
     train_dataframes = info.get_train_dataframes(data_info)
     if args.eval:
-        test_dataframe = list(info.get_test_dataframes(data_info))[0]
-        predictor = Predictor()
-        predictor.fit(train_dataframes)
-        predictions = predictor.predict(test_dataframe)
-        write_predictions(predictios)
+        train_data = pandas.concat([data for name, data in train_dataframes.items()], ignore_index=True)
+        predictor = Predictor(functional_test=args.eval)
+        predictor.fit(train_data)
+        test_dataframes = info.get_test_dataframes(data_info)
+        for fname, test_data in test_dataframes.items():
+            print('start predicting for {} {} {}'.format(fname, len(test_data), len(train_data)))
+            predictions = predictor.predict(test_data)
+            test_data['tags'] = predictions
+            test_data['tags'] = test_data['tags'].apply(' '.join)
+            write_predictions(fname, test_data)
     else:
         avg_score = 0
         for fname, test_data in sorted(train_dataframes.items()):
 
             train_data = pandas.concat([data for name, data in train_dataframes.items() if name!=fname], ignore_index=True)
             print('start learning for {} {} {}'.format(fname, len(test_data), len(train_data)))
-            predictor = Predictor()
+            predictor = Predictor(functional_test=args.test)
             predictor.fit(train_data)
             predictions = predictor.predict(test_data)
             all_tags = test_data['tags']
-            avg_score += get_scoring(predictions, all_tags)
+            score = get_scoring(predictions, all_tags)
+            avg_score += score
+            print(score)
         print(avg_score/len(train_dataframes))
     
 
