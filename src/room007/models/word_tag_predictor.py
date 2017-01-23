26a14adf0bd15ed4d46968e81e25b22e2bd63c21
Jeroen Decroos <jeroen.decroos@gmail.com>
2017-01-23 21:36:20 +0100

    implement features for word tag predict

--- src/room007/models/word_tag_predictor.py
+++ src/room007/models/word_tag_predictor.py
@@ -2,10 +2,25 @@
 # vim: set fileencoding=utf-8 :
 
 import re
+import collections
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
+def remove_numbers(text):
+    return re.sub('[0-9]', '', text)
+
+
+def do_extra_cleaning(data):
+    data['titlecontent'] = data['titlecontent'].map(remove_numbers)
+
+
+def apply_preprocessing(data):
+    if 'tags' in data:
+        data['tags'] = data['tags'].str.split()
+    data['titlecontent'] = data['title'] + data['content']
+    do_extra_cleaning(data)
 
 
 class Predictor(object):
@@ -16,7 +31,7 @@ class Predictor(object):
     def fit(self, train_data):
         print('start fitting')
         if self.functional_test:
-            train_data, throw_away = train_test_split(train_data, test_size=0.99)
+            train_data, throw_away = train_test_split(train_data, test_size=0.9999)
         apply_preprocessing(train_data)
         self._fit(train_data)
 
@@ -44,17 +59,30 @@ class Predictor(object):
         truths = self._get_truths_per_word(train_data)
         self._learn(features, truth)
 
+
     def _train_ti_idf_vectorizer(self, train_data):
         self.ti_idf_vectorizer = TfidfVectorizer(stop_words='english')
-        train_features = self.ti_idf_vectorizer.fit(train_data['titlecontent'])
+        self.ti_idf_vectorizer.fit(train_data['titlecontent'])
+        if self.functional_test:
+            self._write_example_it_idf_features(train_data)
 
     def _get_features_per_word(self, train_data):
-        import pdb; pdb.set_trace()
-        vec = self.ti_idf_vectorizer
-        features = self.ti_idf_vectorizer.transform(train_data['titlecontent'])
-        features2 = [self.ti_idf_vectorizer.transform([x]) for x in train_data.titlecontent.values[0].split()]
+        features = collections.OrderedDict()
+        ti_idf = self._get_ti_idf_features_per_word(train_data)
+        features['ti_idf'] = ti_idf
+        self._write_some_features(features)
+        return features
 
-
+    def _get_ti_idf_features_per_word(self, train_data):
+        ti_idf_data = self.ti_idf_vectorizer.transform(train_data['titlecontent'])
+        feature_names = self.ti_idf_vectorizer.get_feature_names()
+        features = []
+        for i, titlecontent in enumerate(train_data.titlecontent.values):
+            words = titlecontent.split()
+            ti_idf_values = ti_idf_data[i].toarray()[0]
+            pf = [ti_idf_values[feature_names.index(word)] if word in feature_names else 0 for word in words]
+            features.extend(pf)
+        return features
 
 
     def _predict_for_one_entry(self, entry):
@@ -65,6 +93,7 @@ class Predictor(object):
                 prediction.add(word)
         return list(prediction)
 
+
     def _align_prediction(self, prediction, entry):
         if 'tags' in entry:
             diff_length = len(prediction) - len(entry['tags'])
@@ -73,17 +102,26 @@ class Predictor(object):
             if diff_length < 0:
                 prediction.extend(['']*(-1*diff_length))
 
+    def _write_example_it_idf_features(self, train_data):
+        words = train_data.titlecontent.values[0].split()
+        features_one = [self.ti_idf_vectorizer.transform([x]).toarray() for x in words]
+        features_two = self.ti_idf_vectorizer.transform([' '.join(words)]).toarray()
+        self.feature_names = self.ti_idf_vectorizer.get_feature_names()
+        with open('debug_files/it_idf_feats_per_word', 'wt') as outstream:
+            outstream.write(' '.join(train_data.tags.values[0]) + '\n')
+            outstream.write(' '.join(words) + '\n')
+            for feat in features_one:
+                feat = feat[0]
+                outstream.write('A: ')
+                for i, value in enumerate(feat):
+                    if value != 0:
+                        name = self.feature_names[i]
+                        outstream.write('{}:{}:{}:{} , '.format(i, features_two[0][i], name, value))
+                outstream.write('\n')
 
-def remove_numbers(text):
-    return re.sub('[0-9]', '', text)
+    def _write_some_features(self, features):
+        with open('debug_files/feats_per_word', 'wt') as outstream:
+            outstream.write(','.join(features.keys()))
+            for i in range(10):
+                outstream.write(','.join(str(f[i]) for f in features.values()) + '\n')
 
-
-def do_extra_cleaning(data):
-    data['titlecontent'] = data['titlecontent'].map(remove_numbers)
-
-
-def apply_preprocessing(data):
-    if 'tags' in data:
-        data['tags'] = data['tags'].str.split()
-    data['titlecontent'] = data['title'] + data['content']
-    do_extra_cleaning(data)
