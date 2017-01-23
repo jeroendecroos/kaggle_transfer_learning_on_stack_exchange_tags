e834c664d1631348a8c3248c9df0c787d100138c
jdcroos <jd_komoon@hotmail.com>
2017-01-23 09:05:36 -0800

    add init blueprint for word_tag_predictor

--- src/room007/models/word_tag_predictor.py
+++ src/room007/models/word_tag_predictor.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8 :
 
-from collections import Counter
-from functools import partial
-from itertools import chain
 import re
-import argparse
 
-import numpy
-import pandas as pandas
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.metrics import f1_score
-from sklearn.decomposition import NMF
-
-from room007.data import info
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-TAG_STATS_IDX = pandas.Index(('present', 'total'))
 
 
 
 class Predictor(object):
     def __init__(self, functional_test=False):
         self.functional_test = functional_test
+        self.ti_idf_vectorizer = None
 
     def fit(self, train_data):
         print('start fitting')
-        apply_preprocessing(train_data)
         if self.functional_test:
             train_data, throw_away = train_test_split(train_data, test_size=0.99)
+        apply_preprocessing(train_data)
+        self._fit(train_data)
 
     def predict(self, test_dataframe):
         print('start predicting')
@@ -44,17 +30,35 @@ class Predictor(object):
             times += 1
             number_of_tags = 3
             if self.functional_test and times > 10:
-                prediction = []
+                prediction = self._predict_for_one_entry(entry)
             else:
                 prediction = []
-            if 'tags' in entry:
-                diff_length = len(prediction) - len(entry['tags'])
-                if diff_length > 0:
-                    entry['tags'].extend(['']*diff_length)
-                if diff_length < 0:
-                    prediction.extend(['']*(-1*diff_length))
+            self._align_prediction(prediction, entry)
             predictions.append(prediction)
         return predictions
+
+
+    def _fit(self, train_data):
+        self.train_ti_idf_vectorizer(train_data)
+        features = get_features_per_word(train_data)
+        truths = get_truths_per_word(train_data)
+        self.learn(features, truth)
+
+    def _predict_for_one_entry(self, entry):
+        prediction = set()
+        for word in entry['titlecontent']:
+            features = get_features_for_word(word)
+            if self._predict_if_tag(word):
+                prediction.add(word)
+        return list(prediction)
+
+    def _align_prediction(self, prediction, entry):
+        if 'tags' in entry:
+            diff_length = len(prediction) - len(entry['tags'])
+            if diff_length > 0:
+                entry['tags'].extend(['']*diff_length)
+            if diff_length < 0:
+                prediction.extend(['']*(-1*diff_length))
 
 
 def remove_numbers(text):
