74f5504b385e1825d73234a79384afb3fa0965b7
jdcroos <jd_komoon@hotmail.com>
2017-01-24 07:54:01 -0800

    make features seperate from word_tag_predictor

--- src/room007/models/word_tag_predictor.py
+++ src/room007/models/word_tag_predictor.py
@@ -10,49 +10,18 @@ from sklearn import linear_model
 from pandas import DataFrame
 
 
-
-
-class Predictor(object):
+class Features(object):
     def __init__(self, functional_test=False):
         self.functional_test = functional_test
         self.ti_idf_vectorizer = None
 
-
     def fit(self, train_data):
-        print('start fitting')
-        if self.functional_test:
-            train_data, throw_away = train_test_split(train_data, test_size=0.9999)
-        self._fit(train_data)
-
-
-    def predict(self, test_dataframe):
-        print('start predicting')
-        predictions = []
-        times = 0
-        for entry in test_dataframe.to_dict(orient='records'):
-            ## TODO: probably be made faster by using panda tricks and mass transform iso one transform per entry
-            times += 1
-            if self.functional_test and times < 10:
-                prediction = self._predict_for_one_entry(entry)
-            else:
-                break
-                prediction = []
-            self._align_prediction(prediction, entry)
-            predictions.append(prediction)
-        return predictions
-
-
-    def _fit(self, train_data):
         self._train_ti_idf_vectorizer(train_data)
-        features = self._get_features_per_word(train_data)
-        truths = self._get_truths_per_word(train_data)
-        self._learn(features, truths)
 
-    def _learn(self, features, truths):
-        self.logreg = linear_model.LogisticRegression(C=1e5)
-        # we create an instance of Neighbours Classifier and fit the data.
+    def transform(self, train_data):
+        features = self._get_features_per_word(train_data)
         features = [ [f[i] for f in features.values()] for i in range(len(features['ti_idf']))]
-        self.logreg.fit(features, truths)
+        return features
 
     def _train_ti_idf_vectorizer(self, train_data):
         self.ti_idf_vectorizer = TfidfVectorizer(stop_words='english')
@@ -78,38 +47,6 @@ class Predictor(object):
             features.extend(pf)
         return features
 
-    def _get_truths_per_word(self, train_data):
-        truths = []
-        for i, titlecontent in enumerate(train_data.titlecontent.values):
-            words = titlecontent.split()
-            tags = train_data.tags.values[i]
-            truths.extend(w in tags for w in words)
-        return truths
-
-    def _predict_for_one_entry(self, entry):
-        prediction = set()
-        data = DataFrame.from_dict(entry)
-        features = self._get_features_per_word(data)
-        features = [ [f[i] for f in features.values()] for i in range(len(features['ti_idf']))]
-        predictions = self.logreg.predict(features)
-        words = data.titlecontent.values[0].split()
-        for pred, word in zip(predictions, words):
-            if pred:
-                prediction.add(word)
-        return list(prediction)
-
-    def _predict_if_tag(features):
-        # we create an instance of Neighbours Classifier and fit the data.
-        features = [ [f[i] for f in features.values()] for i in range(len(features['ti_idf']))]
-
-    def _align_prediction(self, prediction, entry):
-        if 'tags' in entry:
-            diff_length = len(prediction) - len(entry['tags'])
-            if diff_length > 0:
-                entry['tags'].extend(['']*diff_length)
-            if diff_length < 0:
-                prediction.extend(['']*(-1*diff_length))
-
     def _write_example_it_idf_features(self, train_data):
         words = train_data.titlecontent.values[0].split()
         features_one = [self.ti_idf_vectorizer.transform([x]).toarray() for x in words]
@@ -132,4 +69,70 @@ class Predictor(object):
             outstream.write(','.join(features.keys()))
             for i in range(10):
                 outstream.write(','.join(str(f[i]) for f in features.values()) + '\n')
+
+
+class Predictor(object):
+    def __init__(self, functional_test=False):
+        self.functional_test = functional_test
+
+    def fit(self, train_data):
+        print('start fitting')
+        if self.functional_test:
+            train_data, throw_away = train_test_split(train_data, test_size=0.9999)
+        self._fit(train_data)
+
+    def predict(self, test_dataframe):
+        print('start predicting')
+        predictions = []
+        times = 0
+        for entry in test_dataframe.to_dict(orient='records'):
+            ## TODO: probably be made faster by using panda tricks and mass transform iso one transform per entry
+            times += 1
+            if self.functional_test and times < 10:
+                prediction = self._predict_for_one_entry(entry)
+            else:
+                break
+                prediction = []
+            self._align_prediction(prediction, entry)
+            predictions.append(prediction)
+        return predictions
+
+
+    def _fit(self, train_data):
+        self.feature_creator = Features()
+        self.feature_creator.fit(train_data)
+        features = self.feature_creator.transform(train_data)
+        truths = self._get_truths_per_word(train_data)
+        self._learn(features, truths)
+
+    def _learn(self, features, truths):
+        self.logreg = linear_model.LogisticRegression(C=1e5)
+        self.logreg.fit(features, truths)
+
+    def _get_truths_per_word(self, train_data):
+        truths = []
+        for i, titlecontent in enumerate(train_data.titlecontent.values):
+            words = titlecontent.split()
+            tags = train_data.tags.values[i]
+            truths.extend(w in tags for w in words)
+        return truths
+
+    def _predict_for_one_entry(self, entry):
+        prediction = set()
+        data = DataFrame.from_dict(entry)
+        features = self.feature_creator.transform(data)
+        predictions = self.logreg.predict(features)
+        words = data.titlecontent.values[0].split()
+        for pred, word in zip(predictions, words):
+            if pred:
+                prediction.add(word)
+        return list(prediction)
+
+    def _align_prediction(self, prediction, entry):
+        if 'tags' in entry:
+            diff_length = len(prediction) - len(entry['tags'])
+            if diff_length > 0:
+                entry['tags'].extend(['']*diff_length)
+            if diff_length < 0:
+                prediction.extend(['']*(-1*diff_length))
 
