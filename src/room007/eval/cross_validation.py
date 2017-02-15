0f022a3f2b938be5e48ee83012a7a6ab5dae121a
ilya_kashkarev <ilya.kashkarev@nuance.com>
2017-02-15 10:30:14 +0100

    mean of 2 f-scores seems to be the right metric, changing it

--- src/room007/eval/cross_validation.py
+++ src/room007/eval/cross_validation.py
@@ -20,9 +20,12 @@ def evaluate(expected, predicted):
                 reduce(set.union, predicted, set()))
     coder = MultiLabelBinarizer(sparse_output=True)
     coder.fit([all_tags])
-    return f1_score(coder.transform(expected),
-                    coder.transform(predicted),
-                    average='macro')
+    predicted_enc = coder.transform(predicted)
+    expected_enc = coder.transform(expected)
+
+    # this seems to co-relate with the official evaluation the best
+    return (f1_score(predicted_enc, expected_enc, average='weighted') +
+            f1_score(expected_enc, predicted_enc, average='weighted')) / 2
 
 
 def cross_validate(learner, frames):
