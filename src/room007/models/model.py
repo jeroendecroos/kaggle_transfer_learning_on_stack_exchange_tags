6af1330938524e0726ced02ca85138cfc4643445
Matěj Korvas <matej.korvas@nuance.com>
2017-02-19 23:03:57 +0100

    Impl support for cmdline kwargs

--- src/room007/models/model.py
+++ src/room007/models/model.py
@@ -1,5 +1,5 @@
 # vim: set fileencoding=utf-8 :
-import itertools
+from itertools import chain, product
 
 
 class Option(object):
@@ -25,18 +25,29 @@ class OptionsSetter(object):
     def combinations(self, arg_options):
         """ get all combinations of all options possible, filtered with arg_options
         for the moment filtering is not implemented
+
+        :param Mapping arg_options: mapping from option names to values, to
+            which the combinations should be filtered
+
         """
+
         def option_combination_name(options):
             template = "|||" + "{}:{}&"*len(options) + " |||"
-            return template.format(*itertools.chain(*options.items()))
-        option_choices = [option.choices.keys() for option in self.options.values()]
-        option_combinations = [dict(zip(self.options.keys(), combination))
-                               for combination in itertools.product(*option_choices)
-                               ]
-        if option_combinations[0]:
-            return [(option_combination_name(combination), combination) for combination in option_combinations]
+            return template.format(*chain.from_iterable(options.items()))
+
+        opt_names, opt_values = zip(*((name, option.choices.keys())
+                                    for name, option in self.options.items()))
+        all_combinations = (dict(zip(opt_names, combination))
+                            for combination in product(*opt_values))
+        filtered = [combination for combination in all_combinations
+                    if all(combination[name] == value
+                           for name, value in arg_options.items())]
+        if filtered[0]:
+            return [(option_combination_name(combination), combination)
+                    for combination in filtered]
         else:
-            return [('||| no parameter combinations |||', {option_name: option.default for option_name, option in self.options.items()})]
+            return [('||| no parameter combinations |||',
+                     {option_name: option.default for option_name, option in self.options.items()})]
 
 class Predictor(object):
     OptionsSetter = OptionsSetter
