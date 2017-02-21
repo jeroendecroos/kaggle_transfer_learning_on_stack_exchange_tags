18d7ae2bde650ef029880570d1783923f133cbda
jdcroos <jd_komoon@hotmail.com>
2017-02-21 09:45:34 -0800

    add info message for not processing test_data

--- src/room007/features/build_features.py
+++ src/room007/features/build_features.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8 :
+import logging
 
 from room007.models import word_tag_predictor
 from room007.models import advanced_train_and_predict
@@ -15,6 +16,9 @@ feature_creator.save = True
 for name, dataframe in train_dataframes.items():
     feature_creator.save_features_to_dataframe(dataframe)
 info.save_training_data(data_info, train_dataframes)
-for name, dataframe in test_dataframes.items():
-    feature_creator.save_features_to_dataframe(dataframe)
-info.save_test_data(data_info, test_dataframes)
+if test_dataframes:
+    for name, dataframe in test_dataframes.items():
+        feature_creator.save_features_to_dataframe(dataframe)
+    info.save_test_data(data_info, test_dataframes)
+else:
+    logging.info("didn't save the test data, specify --eval if you also want to save that")
